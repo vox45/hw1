5bf276730e86df02e7ef082d06e3225f1c0e3958
Dave <157144137+vox45@users.noreply.github.com>
2024-02-04 18:27:14 +0000

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,197 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+
+//1
+//   public static void main(String[] args) {
+//    int a = 1; //1,0,-3
+//    if (a==0) {
+//        System.out.println("Right");
+//    } else {
+//        System.out.println("Wrong");
+//    }
+//}
+
+//2
+//   public static void main(String[] args) {
+//    int a = 1; //1,0,-3
+//    if (a>0) {
+//        System.out.println("Right");
+//    } else {
+//        System.out.println("Wrong");
+//    }
+//}
+
+//3
+//   public static void main(String[] args) {
+//    int a = 1; //1,0,-3
+//    if (a<0) {
+//        System.out.println("Right");
+//    } else {
+//        System.out.println("Wrong");
+//    }
+//}
+
+//4
+//   public static void main(String[] args) {
+//    int a = 1; //1,0,-3
+//    if (a==0 || a>0) {
+//        System.out.println("Right");
+//    } else {
+//        System.out.println("Wrong");
+//    }
+//}
+
+//5
+//   public static void main(String[] args) {
+//    int a = 1; //1,0,-3
+//    if (a<0 || a==0) {
+//        System.out.println("Right");
+//    } else {
+//        System.out.println("Wrong");
+//    }
+//}
+
+//6
+//   public static void main(String[] args) {
+//    int a = 1; //1,0,-3
+//    if (a!=0) {
+//        System.out.println("Right");
+//    } else {
+//        System.out.println("Wrong");
+//    }
+// }
+
+//7
+//   public static void main(String[] args) {
+//    int a = 1; //1,0,-3
+//    int test = 1;
+//    if (a==test) {
+//        System.out.println("Right");
+//    } else {
+//        System.out.println("Wrong");
+//    }
+//}
+
+//8
+//   public static void main(String[] args) {
+//    String a = "1"; //"1",1,3
+//    if (a=="1") {
+//        System.out.println("Right");
+//    } else {
+//        System.out.println("Wrong");
+//    }
+//    boolean a = true;
+//    if (a=true) {
+//        System.out.println("Right");
+//    } else {
+//        System.out.println("Wrong");
+//    }
+//}
+
+//9
+//   public static void main(String[] args) {
+//       boolean a = true;
+//       if (a=true) {
+//           System.out.println("Right");
+//       } else {
+//           System.out.println("Wrong");
+//       }
+//}
+
+//10
+//   public static void main(String[] args) {
+//     boolean a = true;
+//       System.out.println(a ? "Right" : "Wrong");
+//}
+
+//   public static void main(String[] args) {
+//       boolean a = true;
+//       if (a!=true) {
+//           System.out.println("Right");
+//       } else {
+//           System.out.println("Wrong");
+//       }
+//}
+
+//11
+//   public static void main(String[] args) {
+//     boolean a = true;
+//       System.out.println(a ? "Wrong" : "Right");
+//}
+
+//   public static void main(String[] args) {
+//        int a = 5;
+//        if (a<5 && a>0) {
+//            System.out.println("Right");
+//        } else {
+//            System.out.println("Wrong");
+//        }
+//}
+
+//12
+//   public static void main(String[] args) {
+//        int a = 5;
+//        if (a==0 || a==2) {
+//            a = a + 7;
+//            System.out.println(a);
+//        } else {
+//            a = a / 10;
+//            System.out.println(a);
+//        }
+//}
+
+//13
+//   public static void main(String[] args) {
+//            int a = 1;
+//            int b = 1;
+//            if (a == 1 || a < 1) {
+//                if(b>3 || b==3) {
+//                    System.out.println(a + b);
+//                } else {
+//                    System.out.println(a - b);
+//                }
+//            } else {
+//                System.out.println(a - b);
+//            }
+//}
+
+//14
+//   public static void main(String[] args) {
+//            int a = 12;
+//            int b = 13;
+//            if (a > 2 && a < 11) {
+//                System.out.println("Right");
+//            } else if(b > 6 && b < 14 || b == 6 && b < 14) {
+//                System.out.println("Right");
+//            } else {
+//                System.out.println("Wrong");
+//            }
+//}
+
+//15
+//   public static void main(String[] args) {
+//                int num = 2;
+//                String result = "";
+//                switch(num) {
+//                    case 1:
+//                        result = "зима";
+//                        break;
+//                    case 2:
+//                        result = "весна";
+//                        break;
+//                    case 3:
+//                        result = "лето";
+//                        break;
+//                    case 4:
+//                        result = "осень";
+//                        break;
+//                }
+//}
+
+
+
+
+
+}
